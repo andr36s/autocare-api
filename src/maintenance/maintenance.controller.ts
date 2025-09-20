327750a6bf76760c04762d2850f91459128f3959
Andrés Gómez <71893648+andr36s@users.noreply.github.com>
2025-09-20 18:20:29 -0500

    agrego

--- src/maintenance/maintenance.controller.ts
+++ src/maintenance/maintenance.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put, Req } from '@nestjs/common';
 
 @Controller('maintenance')
-export class MaintenanceController {}
+export class MaintenanceController {
+
+    @Get() // Sirve para consultar
+    methodGet(@Req() req: Request) {
+        return `method ${req.method} called`;
+    }
+
+    @Post(':id') // Sirve para crear
+    methodPost(@Param('id') id: string) {
+        return `id ${id}.`;
+    }
+
+    @Put() // Sirve para actualizar total
+    methodPut(@Req() req: Request) {
+        return `Method ${req.method} called`;
+    }
+
+    @Patch() // Sirve para actualizar parcial
+    methodPatch(@Req() req: Request) {
+        return `Method ${req.method} called`;
+    }
+
+    @Delete() // Sirve para eliminar
+    methodDelete(@Req() req: Request) {
+        return `Method ${req.method} called`;
+    }
+}
