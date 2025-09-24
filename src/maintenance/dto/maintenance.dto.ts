3f668f5c44359d91798bf7ebf06b85a5f2e058df
Andrés Gómez <71893648+andr36s@users.noreply.github.com>
2025-09-24 17:53:52 -0500

    maintenance - configuro el DTO para que realice validaciones de class-validator

--- src/maintenance/dto/maintenance.dto.ts
+++ src/maintenance/dto/maintenance.dto.ts
@@ -1,4 +1,11 @@
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
 export class MaintenanceDTO {
+    @IsNotEmpty()
+    @IsString()
     readonly description: string;
+
+    @IsNotEmpty()
+    @IsBoolean()
     readonly isDone: boolean;
 }
